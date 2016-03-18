78d992263fa80559a8284db88aeaf67aa91a600a
Adrian Lock <admin@dropboxexplorer.com>
2016-03-18 16:11:01 +0000

    Initial check-in

---
+++ DropBoxExplorer/UI/DropboxDialogBase.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DropboxExplorer
+{
+    /// <summary>
+    /// The base class used for displaying Dropbox dialog boxes on the screen.
+    /// </summary>
+    public abstract partial class DropboxDialogBase : Form
+    {
+        #region Enums
+        internal enum DialogMode
+        {
+            Open,
+            Save
+        }
+        #endregion
+        
+        #region Member variables
+        private DialogMode _Mode = DialogMode.Open;
+
+        private string _UploadFile = "";
+        #endregion
+
+        #region Public interface
+        /// <summary>
+        /// The full Dropbox path to the currently selected file.
+        /// </summary>
+        public string SelectedFile { get; private set; }
+
+        /// <summary>
+        /// If set, the Open dialog will automatically download the chosen file to this local folder
+        /// </summary>
+        public string DownloadFolder { get; set; }
+
+        /// <summary>
+        /// The full local path to the downloaded file. If a file with the same name already exists, a new unique name will be generated.
+        /// </summary>
+        public string DownloadedFile { get; private set; }
+
+        /// <summary>
+        /// The full local path of a file the Save dialog should uploaded to the chosen location.
+        /// </summary>
+        public string UploadFile
+        {
+            get { return _UploadFile; }
+            set
+            {
+                _UploadFile = value;
+                txtFilename.Text = System.IO.Path.GetFileName(_UploadFile);
+            }
+        }
+
+        /// <summary>
+        /// Downloads the selected Dropbox file to the provided local path. If the file exists it will be overwritten.
+        /// </summary>
+        /// <param name="localFilePath">The full local path to download the file to.</param>
+        /// <returns>An asynchronous operation result.</returns>
+        public async Task DownloadSelectedFile(string localFilePath)
+        {
+            if (string.IsNullOrEmpty(this.SelectedFile))
+                throw new Exception("No file selected");
+            
+            await fileTransfer1.DownloadFileUI(this.SelectedFile, localFilePath);
+        }
+
+        /// <summary>
+        /// Uploads the provided local file to the selected Dropbox target.
+        /// </summary>
+        /// <param name="localFilePath">The full local path to the file to upload to the current Dropbox target. Dropbox will generate a new file name if required.</param>
+        /// <returns>An asynchronous operation result.</returns>
+        public async Task UploadFileToCurrentFolder(string localFilePath)
+        {
+            if (string.IsNullOrEmpty(localFilePath))
+                throw new Exception("No file provided");
+            if (!System.IO.File.Exists(localFilePath))
+                throw new Exception("Local file not found");
+            
+            string dropboxFilePath = System.IO.Path.Combine(fileBrowser1.Path, txtFilename.Text);
+            dropboxFilePath = dropboxFilePath.Replace(@"\", "/");
+
+            if (!dropboxFilePath.StartsWith("/"))
+                dropboxFilePath = "/" + dropboxFilePath;
+            
+            await fileTransfer1.UploadFileUI(dropboxFilePath, localFilePath);
+        }
+        #endregion
+
+        #region Constructor
+        internal DropboxDialogBase(DialogMode mode)
+        {
+            SelectedFile = "";
+            _Mode = mode;
+
+            InitializeComponent();
+            this.Text = _Mode.ToString();
+            btnOK.Text = _Mode.ToString();
+
+            SetFormState(false);
+
+            fileBrowser1.Initialise();
+        }
+        #endregion
+
+        #region Event handlers
+        #region File Browser
+        private void fileBrowser1_FileSelected(object sender, FileBrowser.ItemSelectedArgs e)
+        {
+            txtFilename.Text = System.IO.Path.GetFileName(e.Path);
+        }
+
+        private void fileBrowser1_FileDoubleClicked(object sender, FileBrowser.ItemSelectedArgs e)
+        {
+            FileSelected();
+        }
+        #endregion
+        
+        #region Buttons
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            FileSelected();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+
+        #region File name textbox
+        private void txtFilename_TextChanged(object sender, EventArgs e)
+        {
+            string file = System.IO.Path.GetFileName(fileBrowser1.GetSelectedFilePath());
+            if (txtFilename.Text != file)
+                fileBrowser1.ClearSelection();
+        }
+        #endregion
+        #endregion
+
+        #region Private methods
+        private async Task DownloadSelectedFile()
+        {
+            if (string.IsNullOrEmpty(this.DownloadFolder))
+                return;
+
+            if (string.IsNullOrEmpty(this.SelectedFile))
+                throw new Exception("No file selected");
+
+            fileTransfer1.Show();
+            fileBrowser1.Hide();
+
+            SetFormState(false);
+            this.DownloadedFile = GetUniqueLocalFileName();
+            await DownloadSelectedFile(this.DownloadedFile);
+            SetFormState(true);
+        }
+
+        private async Task UploadSelectedFile()
+        {
+            if (string.IsNullOrEmpty(this.UploadFile))
+                return;
+            
+            fileTransfer1.Show();
+            fileBrowser1.Hide();
+
+            SetFormState(false);
+            await UploadFileToCurrentFolder(this.UploadFile);
+            SetFormState(true);
+        }
+
+        private async void FileSelected()
+        {
+            SelectedFile = fileBrowser1.GetSelectedFilePath();
+            if (_Mode == DialogMode.Open && !string.IsNullOrEmpty(SelectedFile))
+            {
+                await DownloadSelectedFile();
+                this.DialogResult = DialogResult.OK;
+            }
+            else if (_Mode == DialogMode.Save && txtFilename.Text.Length > 0)
+            {
+                await UploadSelectedFile();
+                this.DialogResult = DialogResult.OK;
+            }
+        }
+
+        private string GetUniqueLocalFileName()
+        {
+            string fileName = System.IO.Path.GetFileName(this.SelectedFile);
+            string localFilePath = System.IO.Path.Combine(this.DownloadFolder, fileName);
+
+            string name = System.IO.Path.GetFileNameWithoutExtension(localFilePath);
+            string ext = System.IO.Path.GetExtension(localFilePath);
+
+            int index = 0;
+            while (true)
+            {
+                if (!System.IO.File.Exists(localFilePath))
+                    break;
+
+                index++;
+                fileName = string.Format("{0} ({1}){2}", name, index, ext);
+                localFilePath = System.IO.Path.Combine(this.DownloadFolder, fileName);
+            }
+
+            return localFilePath;
+        }
+
+        private void SetFormState(bool enabled)
+        {
+            lblFilename.Enabled = enabled;
+            txtFilename.Enabled = enabled;
+            btnOK.Enabled = enabled;
+        }
+        #endregion
+    }
+}
---
+++ DropBoxExplorer/UI/FileListing.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Dropbox.Api.Files;
+
+namespace DropboxExplorer
+{
+    /// <summary>
+    /// Provides a simple file list control using a ListView
+    /// </summary>
+    internal partial class FileListing : UserControl
+    {
+        private string _CurrentPath = "";
+
+        #region Public events
+        public class ItemSelectedArgs : EventArgs
+        {
+            public enum SelectedItemType
+            {
+                Folder,
+                File
+            }
+
+            /// <summary>
+            /// The type of the item selected
+            /// </summary>
+            public SelectedItemType ItemType { get; internal set; }
+
+            /// <summary>
+            /// The path to the selected item
+            /// </summary>
+            public string Path { get; internal set; }
+        }
+
+        /// <summary>
+        /// An item has been selected
+        /// </summary>
+        public event EventHandler<ItemSelectedArgs> ItemSelected;
+
+        /// <summary>
+        /// An item has been double clicked
+        /// </summary>
+        public event EventHandler<ItemSelectedArgs> ItemDoubleClicked;
+        #endregion
+
+        #region Public interface
+        public FileListing()
+        {
+            InitializeComponent();
+
+            filetypes16.Images.Clear();
+            filetypes16.Images.Add("Folder", Properties.Resources.Folder);
+            filetypes16.Images.Add("File", Properties.Resources.File);
+
+            filetypes48.Images.Clear();
+            filetypes48.Images.Add("Folder", Properties.Resources.Folder48);
+            filetypes48.Images.Add("File", Properties.Resources.File48);
+
+            listview.Columns.Add("Name");
+            listview.Columns.Add("Type");
+            listview.Columns.Add("Size");
+            listview.Columns.Add("Date");
+
+            FillColumnsToWidth();
+            WinAPI.ConfigureListView(listview);
+        }
+        
+        /// <summary>
+        /// Gets the full path to the selected item
+        /// </summary>
+        internal string SelectedItem
+        {
+            get
+            {
+                Metadata item = GetSelectedObject();
+                if (item != null)
+                {
+                    return item.PathLower;
+                }
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// Navigates to the required folder
+        /// </summary>
+        /// <param name="path">The path to navigate to</param>
+        /// <returns>The result of the asynchronous operation</returns>
+        internal async Task NavigateToFolder(string path)
+        {
+            _CurrentPath = path;
+            if (path == null) path = "";
+            if (path.Length > 0 && !path.StartsWith("/")) path = "/" + path;
+            if (path == "/") path = "";
+
+            busyIcon1.Show();
+            busyIcon1.BringToFront();
+            
+            try
+            {
+                listview.Items.Clear();
+
+                using (var dropbox = new DropboxFiles())
+                {
+                    var list = await dropbox.GetFolderContents(path);
+
+                    foreach (var folder in list.Entries.Where(i => i.IsFolder))
+                    {
+                        var item = AddItem(folder.Name, "Folder", "", "File folder", "");
+                        item.Tag = folder;
+                    }
+
+                    foreach (var file in list.Entries.Where(i => i.IsFile))
+                    {
+                        var task = AddFile(dropbox, file.AsFile);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorPanel.ShowError(this, "A problem occurred retrieving folder contents.", ex);
+            }
+            
+            busyIcon1.Hide();
+        }
+
+        /// <summary>
+        /// Clears the current selection
+        /// </summary>
+        internal void ClearSelection()
+        {
+            while (listview.SelectedItems.Count > 0)
+                listview.SelectedItems[0].Selected = false;
+        }
+        #endregion
+
+        #region Event handlers
+        private void listview_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ItemSelected == null) return;
+
+            ItemSelectedArgs args = GetSelectedItemArgs();
+            if (args == null) return;
+
+            ItemSelected(this, args);
+        }
+
+        private void listview_DoubleClick(object sender, EventArgs e)
+        {
+            if (ItemDoubleClicked == null) return;
+
+            ItemSelectedArgs args = GetSelectedItemArgs();
+            if (args == null) return;
+
+            ItemDoubleClicked(this, args);
+        }
+
+        private void menuBrowser_Opening(object sender, CancelEventArgs e)
+        {
+            View view = listview.View;
+            menuTiles.Checked = (view == View.Tile);
+            menuLargeIcons.Checked = (view == View.LargeIcon);
+            menuSmallIcons.Checked = (view == View.SmallIcon);
+            menuList.Checked = (view == View.List);
+            menuDetails.Checked = (view == View.Details);
+        }
+
+        private async void menuBrowserRefresh_Click(object sender, EventArgs e)
+        {
+            await NavigateToFolder(_CurrentPath);
+        }
+
+        private void menuTiles_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem item = sender as ToolStripMenuItem;
+            listview.View = (View)int.Parse(item.Tag.ToString());
+        }
+        #endregion
+
+        #region Private methods
+        private void FillColumnsToWidth()
+        {
+            int width = this.Width / listview.Columns.Count;
+            foreach (ColumnHeader col in listview.Columns)
+            {
+                col.Width = width;
+            }
+        }
+
+        private Metadata GetSelectedObject()
+        {
+            if (listview.SelectedItems.Count > 0)
+            {
+                var item = listview.SelectedItems[0];
+                return item.Tag as Metadata;
+            }
+            return null;
+        }
+
+        private ItemSelectedArgs GetSelectedItemArgs()
+        {
+            Metadata item = GetSelectedObject();
+            if (item != null)
+            {
+                ItemSelectedArgs args = new ItemSelectedArgs();
+                args.ItemType = (item.IsFolder ? ItemSelectedArgs.SelectedItemType.Folder : ItemSelectedArgs.SelectedItemType.File);
+                args.Path = item.PathLower;
+                return args;
+            }
+            return null;
+        }
+
+        private async Task AddFile(DropboxFiles dropbox, FileMetadata file)
+        {
+            const string THUMBNAILS = ".jpg.jpeg.png.tiff.tif.gif.bmp";
+
+            string ext = System.IO.Path.GetExtension(file.Name);
+            string imageKey = ext;
+
+            if (filetypes16.Images.ContainsKey(file.PathLower))
+            {
+                imageKey = file.PathLower;
+            }
+            else if (!filetypes16.Images.ContainsKey(ext))
+            {
+                filetypes16.Images.Add(ext, WinAPI.GetFileSmallIcon(file.Name));
+                filetypes48.Images.Add(ext, WinAPI.GetFileExtraLargeIcon(file.Name));
+            }
+
+            var item = AddItem(file.Name, imageKey, file.AsFile.ClientModified.ToString(), WinAPI.GetFileTypeName(ext), WinAPI.FormatBytes(file.AsFile.Size));
+            item.Tag = file;
+
+            // If current image is based on extension, get the thumbnail
+            if (THUMBNAILS.Contains(ext) && imageKey == ext)
+            {
+                var task = await dropbox.GetThumbnail(file.PathLower);
+                using (var stream = task.GetContentAsStreamAsync())
+                {
+                    using (Bitmap image = new Bitmap(stream.Result))
+                    {
+                        Image thumbnail = GetThumbnail(image, filetypes48.ImageSize.Width);
+                        filetypes16.Images.Add(file.PathLower, WinAPI.GetFileSmallIcon(file.Name));
+                        filetypes48.Images.Add(file.PathLower, thumbnail);
+                        item.ImageKey = file.PathLower;
+                    }
+                }
+            }
+        }
+
+        private ListViewItem AddItem(string name, string icon, string date, string type, string size)
+        {
+            var item = listview.Items.Add(name, icon);
+            item.SubItems.Add(type);
+            item.SubItems.Add(size);
+            item.SubItems.Add(date);
+            return item;
+        }
+
+        private Image GetThumbnail(Image source, int size)
+        {
+            // Calculate our thumbnail size
+            Rectangle ThumbRect = new Rectangle(0, 0, size, size);
+            Rectangle TargetRect = new Rectangle(0, 0, size, size);
+            if (source == null)
+            {
+                // default size
+            }
+            else if (source.Width > source.Height)
+            {
+                TargetRect.Height = (int)((double)source.Height * (double)size / (double)source.Width);
+                TargetRect.Y = ThumbRect.Height - TargetRect.Height;
+            }
+            else if (source.Width < source.Height)
+            {
+                TargetRect.Width = (int)((double)source.Width * (double)size / (double)source.Height);
+                TargetRect.X = ThumbRect.Width - TargetRect.Width;
+            }
+
+            // Create the thumbnail
+            Image thumb = new Bitmap(ThumbRect.Width, ThumbRect.Height);
+            using (Graphics g = Graphics.FromImage(thumb))
+            {
+                g.Clear(Color.FromKnownColor(KnownColor.Window));
+                if (source != null)
+                    g.DrawImage(source, TargetRect);
+            }
+            return thumb;
+        }
+        #endregion
+    }
+}
