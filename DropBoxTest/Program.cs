c27acf696559f81811f23aeb7e56224973541961
Dropbox Explorer <admin@dropboxexplorer.com>
2016-03-18 16:58:21 +0000

    Updated AppKey

--- DropBoxTest/Program.cs
+++ DropBoxTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Windows.Forms;
 
 namespace DropboxExplorerTest
@@ -13,10 +14,31 @@ namespace DropboxExplorerTest
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            
-            DropboxExplorer.Configuration.DropboxAppKey = "7su6xqsuh1msl4w";
+
+
+            DropboxExplorer.Configuration.DropboxAppKey = GetAppKey();
 
             Application.Run(new DropboxExplorer.Test.FormExample());
+        }
+
+        /// <summary>
+        /// Function allows local AppKey to not be in GitHub
+        /// </summary>
+        /// <returns></returns>
+        private static string GetAppKey()
+        {
+            try
+            {
+                RegistryKey regKey = Registry.CurrentUser.OpenSubKey(@"Software\Dropbox Explorer", false);
+                if (regKey != null)
+                {
+                    object value = regKey.GetValue("AppKey", "");
+                    return value.ToString();
+                }
+            }
+            catch { }
+
+            return "";
         }
     }
 }
